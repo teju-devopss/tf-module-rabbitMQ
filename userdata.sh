b39db00fce04aaa1af9dbcfd2e7c01387ed9d94f
tejaswiniforaws <tejaswiniforaws@gmail.com>
2025-08-04 21:15:17 +0530

    rabbitMQ setup

--- userdata.sh
+++ userdata.sh
@@ -7,3 +7,5 @@ systemctl enable rabbitmq-server
 systemctl start rabbitmq-server
 rabbitmqctl add_user roboshop roboshop123
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+
+#manual steps of rabbitmq
