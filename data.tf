3757db212650384e2fefa73081067948803d3096
tejaswiniforaws <tejaswiniforaws@gmail.com>
2025-08-04 21:06:39 +0530

    rabbitMQ setup

--- data.tf
+++ data.tf
@@ -1,5 +1,5 @@
-# data "aws_ami" "centos8" {
-#   most_recent      = true
-#   name_regex       = "Centos-8-DevOps-Practice"
-#   owners           = [973714476881]
-# }
+data "aws_ami" "centos8" {
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
+  owners           = [973714476881]
+}
--- locals.tf
+++ locals.tf
@@ -1,4 +1,4 @@
-# locals {
-#   prefix       = "${var.env}-${var.project_name}"
-#   name         = "${var.env}-${var.project_name}-rabbitmq"
-# }
+locals {
+  prefix       = "${var.env}-${var.project_name}"
+  name         = "${var.env}-${var.project_name}-rabbitmq"
+}
